7c08484493da2a182d2907de83a3ef396fa6eff8
Alibubere <alibubere989@gmail.com>
2025-11-13 22:02:37 +0530

    multiple changes

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import os 
 import logging
-from src.data_fetch import load_config , fetch_stock
-from datetime import datetime
+from src.data_fetcher import fetch_stock
 import json
 
 
 def setup_logging():
     log_dir = "logs"
     os.makedirs(log_dir,exist_ok=True)
-    log_file=os.path.join(log_dir,"Pipeline.log")
+    log_file=os.path.join(log_dir,"pipeline.log")
     logging.basicConfig(
         level=logging.INFO,
         format= "%(asctime)s -%(levelname)s - %(message)s",
@@ -20,37 +19,29 @@ def setup_logging():
     logging.info("Logging Successfully Initialized")
 
 
-def load_config(config_dir="configs/config.yaml"):
-    try:
-        with open (config_dir,"r") as f:
-            config= yaml.safe_load(f)
-            return config
-        
-    except Exception as e :
-        logging.error(f"Error loading config {e}")
-        raise
+def save_raw(symbol , data,output_dir="data"):
+    os.makedirs(output_dir,exist_ok=True)
+    file_path = os.path.join(output_dir,f"raw_data_{symbol}.json")
 
+    try:
+        with open (file_path,"w") as f:
+            json.dump(data,f,indent=4)
+            
+        logging.info(f"Raw data saved at {file_path}")
+    
+    except Exception as e:
+        logging.error(f"Failed to save raw data: {e}")
+       
+
+    
 
 def main():
-
     setup_logging()
+    symbols= ["AAPL", "TSLA", "GOOGL", "BAC"]
+    for symbol in symbols:
+        raw = fetch_stock(symbol=symbol)
+        save_raw(symbol,raw)  
     
-    config=load_config()
-    api_url =config.get("api_url")
-    data= fetch_stock(symbol="BAC",url=api_url)
-
-    if data:
-        timestamp= datetime.now().strftime("%Y-%m-%d_%H-%M")
-        raw_data_path= f"data/raw_data_{timestamp}.json"
-
-        with open(raw_data_path,"w") as f:
-            json.dump(data,f,indent=4)
-        
-        logging.info(f"Data saved successfully at {raw_data_path} ")
-    
-    else:
-        logging.warning("No data fetch")
-
 
 if __name__ == "__main__":
     main()
