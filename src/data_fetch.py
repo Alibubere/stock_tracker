3993665c426c304714c87908599ae9de78443f51
Alibubere <alibubere989@gmail.com>
2025-11-13 20:32:27 +0530

    fetching funtion

--- src/data_fetch.py
+++ src/data_fetch.py
@@ -3,6 +3,8 @@ import logging
 import requests
 from dotenv import load_dotenv
 import yaml
+from datetime import datetime
+import json
 
 load_dotenv()
 
@@ -12,6 +14,8 @@ if API_key is None:
     logging.error("API Key not found. Check your .env file")
 
 
+def setup_logging()
+    log
 
 def load_config(config_dir="configs/config.yaml"):
     try:
@@ -24,7 +28,13 @@ def load_config(config_dir="configs/config.yaml"):
         raise
 
 
-def fetch_multiple_stock_data(symbol_list,api_url="https://www.alphavantage.co/query",params=None):
+def fetch_stock(symbol: str,url = "https://www.alphavantage.co/query"):
+    
+    params = {
+        "function": "TIME_SERIES_DAILY",
+        "symbol": symbol,
+        "apikey": API_KEY
+    }
     try:
         logging.info(f"Fetching data from url {api_url}....")
         response = requests.get(api_url,params=params)
@@ -34,3 +44,27 @@ def fetch_multiple_stock_data(symbol_list,api_url="https://www.alphavantage.co/q
     except Exception as e :
         logging.error(f"Error Fetching data: {e}")
         return None
+
+def main():
+    config=load_config()
+    api_url =config.get("api_url")
+    data= fetch_stock(symbol="BAC",url=api_url)
+
+    if data:
+        timestamp= datetime.now().strftime("%Y-%m-%d_%H-%M")
+        raw_data_path= f"data/raw_data_{timestamp}.json"
+
+        with open(raw_data_path,"w") as f:
+            json.dump(data,f,indent=4)
+        
+        logging.INFO(f"Data saved successfully at {raw_data_path} ")
+    
+    else:
+        logging.warning("No data fetch")
+
+    
+
+
+
+if __name__ == "__main__":
+    main()
