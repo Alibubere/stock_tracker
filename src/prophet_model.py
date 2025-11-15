2db5552c8d828d85ce67884dddf5e757e4b493ab
Alibubere <alibubere989@gmail.com>
2025-11-15 12:26:03 +0530

    model train

--- src/prophet_model.py
+++ src/prophet_model.py
@@ -1,27 +1,33 @@
 import logging
 import pandas as pd
+from prophet import Prophet
+
 
 def prepare_for_prophet(df):
 
     if df is None:
         logging.error("Input DataFrame is None — cannot prepare for Prophet.")
         return None
-     
+
     if "date" not in df.columns or "close" not in df.columns:
         logging.error("Dataframe missing required columns ['date','close']")
         return None
-    
+
     try:
 
-        df = df.sort_values("date",ascending= True)
+        df = df.sort_values("date", ascending=True)
 
-        prophet_df = df[["date","close"]]
+        prophet_df = df[["date", "close"]]
 
-        prophet_df=prophet_df.rename(columns={"date":"ds" , "close":"y"}) 
+        prophet_df = prophet_df.rename(columns={"date": "ds", "close": "y"})
 
-        prophet_df["y"]=prophet_df["y"].astype(float)
+        prophet_df["y"] = prophet_df["y"].astype(float)
 
-        prophet_df["ds"]=pd.to_datetime(prophet_df["ds"])
+        prophet_df["ds"] = pd.to_datetime(prophet_df["ds"])
+
+        prophet_df = prophet_df.dropna(subset=["ds", "y"])
+
+        prophet_df.reset_index(drop=True, inplace=True)
 
         return prophet_df
 
@@ -30,4 +36,28 @@ def prepare_for_prophet(df):
         return None
 
 
+def train_prophet_model(prophet_df):
 
+    if prophet_df is None:
+        logging.error("Prophet data frame is NONE - cannot train prophet model")
+        return None
+
+    if "ds" not in prophet_df.columns or "y" not in prophet_df.columns:
+        logging.error("Prophet DF missing required columns ['ds','y']")
+        return None
+    try:
+        model = Prophet(
+            daily_seasonality=True, weekly_seasonality=True, yearly_seasonality=False
+        )
+
+        logging.info(f"Training Prophet model on {len(prophet_df)} rows")
+
+        model.fit(prophet_df)
+
+        logging.info("Model trained Successfully")
+
+        return model
+
+    except Exception as e:
+        logging.exception("Failed to train Prophet model")
+        return None
